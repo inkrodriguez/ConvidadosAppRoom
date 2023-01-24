0fd71d7e640495f1300397baaa42062cdb3369c1
Rodrigo <rodrigorasilva@hotmail.com>
2023-01-24 15:48:58 -0300

    Adicionada função de delete e edit

--- app/src/main/java/com/inkrodriguez/convidadosapproom/AddUserActivity.kt
+++ app/src/main/java/com/inkrodriguez/convidadosapproom/AddUserActivity.kt
@@ -7,14 +7,25 @@ import com.inkrodriguez.convidadosapproom.databinding.ActivityAddUserBinding
 import kotlinx.coroutines.launch
 
 class AddUserActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityAddUserBinding
+    private var user: User? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        user = intent.getSerializableExtra("Data") as? User
 
-        binding.btnAddUser.setOnClickListener {
+        if(user == null) binding.btnAddOrUpdateUser.text = "Add User"
+        else {
+            binding.btnAddOrUpdateUser.text = "Update User"
+            binding.edFirstName.setText(user?.firstName.toString())
+            binding.edLastName.setText(user?.lastName.toString())
+        }
+
+        binding.btnAddOrUpdateUser.setOnClickListener {
             addUser()
         }
 
@@ -25,12 +36,18 @@ class AddUserActivity : AppCompatActivity() {
         val lastName = binding.edLastName.text.toString()
 
         lifecycleScope.launch {
-            val user = User(firstName, lastName)
-            AppDataBase(this@AddUserActivity).getUserDao().addUser(user)
-            finish()
+            if (user == null) {
+                val user = User(firstName, lastName)
+                AppDataBase(this@AddUserActivity).getUserDao().addUser(user)
+                finish()
+            } else {
+                val u = User(firstName, lastName)
+                u.id = user?.id ?: 0
+                AppDataBase(this@AddUserActivity).getUserDao().updateUser(u)
+                finish()
+            }
         }
 
-
-
     }
+
 }
--- app/src/main/java/com/inkrodriguez/convidadosapproom/MainActivity.kt
+++ app/src/main/java/com/inkrodriguez/convidadosapproom/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.inkrodriguez.convidadosapproom
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.inkrodriguez.convidadosapproom.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
+
+    private var mAdapter: UserAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +33,49 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setAdapter(list: List<User>){
+        mAdapter?.setData(list)
+    }
+
+
     override fun onResume() {
         super.onResume()
 
         lifecycleScope.launch {
             val userList = AppDataBase(this@MainActivity).getUserDao().getAllUser()
-            Log.e("ooooo", "${userList.size}")
+
+            mAdapter = UserAdapter()
+            binding.recyclerView.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = mAdapter
+                setAdapter(userList)
+
+                    mAdapter?.setOnActionEditListener {
+                        val intent = Intent(this@MainActivity, AddUserActivity::class.java)
+                        intent.putExtra("Data", it)
+                        startActivity(intent)
+                    }
+
+                    mAdapter?.setOnActionDeleteListener {
+                        val builder = AlertDialog.Builder(this@MainActivity)
+                        builder.setMessage("Are you sure you want to delete?")
+                        builder.setPositiveButton("Yes"){ p0, p1 ->
+                            lifecycleScope.launch {
+                                AppDataBase(this@MainActivity).getUserDao().deleteUser(it)
+                                val userList = AppDataBase(this@MainActivity).getUserDao().getAllUser()
+                                setAdapter(userList)
+                            }
+                                p0.dismiss()
+                        }
+                        builder.setNegativeButton("No"){ p0, p1 ->
+                            p0.dismiss()
+                        }
+                        val dialog = builder.create()
+                        dialog.show()
+                    }
+
+
+                }
+            }
         }
     }
-
-}
